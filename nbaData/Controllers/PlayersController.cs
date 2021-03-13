bbf6fb21061c669015a8a780f641a958d0829115
Zach Sarkis <zacherysarkis@gmail.com>
2021-03-13 10:06:55 -0700

    First commit

---
+++ nbaData/Controllers/PlayersController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using nbaData.Models;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Serializers.NewtonsoftJson;
+
+namespace nbaData.Controllers
+{
+    [ApiController]
+    [Route("api/v1/players")]
+    public class PlayersController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Player> GetPlayers()
+        {
+            var players = new List<Player>();
+
+            var client = new RestClient("https://www.balldontlie.io/api/v1/players?page=0&per_page=100") {Timeout = -1};
+            client.UseNewtonsoftJson();
+            var request = new RestRequest(Method.GET);
+            var response = client.Execute(request);
+
+            var result = JsonConvert.DeserializeObject<BallDontLieResponse>(response.Content);
+            players.AddRange(result.data);
+
+            for (var x = 2; x <= result.meta.total_pages; x++)
+            {
+                var loopRequest = new RestRequest($"https://www.balldontlie.io/api/v1/players?page={x}&per_page=100");
+                var loopResponse = client.Execute(loopRequest);
+                var loopResult = JsonConvert.DeserializeObject<BallDontLieResponse>(loopResponse.Content);
+
+                players.AddRange(loopResult.data);
+            }
+
+            return players;
+        }
+    }
+}
---
+++ nbaData/Models/Player.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+
+namespace nbaData.Models
+{
+    public class Player
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public double? height_feet { get; set; }
+        public double? height_inches { get; set; }
+        public string position { get; set; }
+        public double? weight_pounds { get; set; }
+
+        public Team team { get; set; }
+
+        public Player(string firstName, string lastName)
+        {
+            this.first_name = firstName;
+            this.last_name = lastName;
+        }
+    }
+
+    public class Team
+    {
+        public int id { get; set; }
+        public string abbreviation { get; set; }
+        public string city { get; set; }
+        public string conference { get; set; }
+        public string division { get; set; }
+        public string full_name { get; set; }
+        public string name { get; set; }
+    }
+
+    public class BallDontLieResponse
+    {
+        public List<Player> data { get; set; }
+        public Meta meta { get; set; }
+    }
+
+    public class Meta
+    {
+        public int? total_pages { get; set; }
+        public int? current_page { get; set; }
+        public int? next_page { get; set; }
+        public int? per_page { get; set; }
+        public int? total_count { get; set; }
+    }
+}
