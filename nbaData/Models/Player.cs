99f5a9d33ef6154b9fe26b519ac9769d73846e0f
Zach Sarkis <zacherysarkis@gmail.com>
2021-06-29 10:15:28 -0600

    Add ID arg to player constructor as well as ShootingStats

--- nbaData/Models/Player.cs
+++ nbaData/Models/Player.cs
@@ -4,8 +4,15 @@ namespace nbaData.Models
 {
     public class Player
     {
-        public Player(string firstName, string lastName, Team teamName = null, double? heightFeet = null, double? heightInches = null, string position = null, double? weightPounds = null)
+        public Player(string firstName, string lastName, Team teamName = null, 
+            double? heightFeet = null, double? heightInches = null, string position = null, 
+            double? weightPounds = null, int? id = null)
         {
+            if (id != null)
+            {
+                this.id = (int)id;
+            }
+
             first_name = firstName;
             last_name = lastName;
             team = teamName;
@@ -24,6 +31,12 @@ namespace nbaData.Models
         public double? weight_pounds { get; set; }
 
         public Team team { get; set; }
+        
+        public ShootingStats shootingStatsShortTerm { get; set; }
+        
+        public ShootingStats shootingStatsMidTerm { get; set; }
+        
+        public ShootingStats shootingStatsLongTerm { get; set; }
     }
 
     public class Team
@@ -35,6 +48,14 @@ namespace nbaData.Models
         public string division { get; set; }
         public string full_name { get; set; }
         public string name { get; set; }
+    }
+
+    public class ShootingStats
+    {
+        public double threePointVolume { get; set; }
+        public double threePointPercentage { get; set; }
+        public double twoPointVolume { get; set; }
+        public double twoPointPercentage { get; set; }
     }
 
     public class BallDontLieResponse
