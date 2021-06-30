b8e7ada487711d3d43bd2ce62f80c33ee2643fca
Zach Sarkis <zacherysarkis@gmail.com>
2021-06-30 07:58:04 -0600

    Add backing field for players after first poll ND-14

--- nbaData/BallDontLieManager.cs
+++ nbaData/BallDontLieManager.cs
@@ -10,66 +10,71 @@ namespace nbaData
 {
     public class BallDontLieManager : IBallDontLieManager
     {
+        public IEnumerable<Player> _players = null;
+
         public IEnumerable<Player> GetPlayers()
         {
             List<Player> players = new List<Player>();
-            
-            RestClient client = new RestClient("https://www.balldontlie.io/api/v1/players?page=1&per_page=100") {Timeout = -1};
+
+            RestClient client = new RestClient("https://www.balldontlie.io/api/v1/players?page=1&per_page=100")
+                {Timeout = -1};
             client.UseNewtonsoftJson();
             RestRequest request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
 
-            BallDontLiePlayerResponse result = JsonConvert.DeserializeObject<BallDontLiePlayerResponse>(response.Content);
+            BallDontLiePlayerResponse result =
+                JsonConvert.DeserializeObject<BallDontLiePlayerResponse>(response.Content);
             players.AddRange(result.data);
 
             for (int x = 2; x <= result.meta.total_pages; x++)
             {
-                RestClient loopClient = new RestClient($"https://www.balldontlie.io/api/v1/players?page={x}&per_page=100") {Timeout = -1};
+                RestClient loopClient =
+                    new RestClient($"https://www.balldontlie.io/api/v1/players?page={x}&per_page=100") {Timeout = -1};
                 RestRequest loopRequest = new RestRequest(Method.GET);
                 IRestResponse loopResponse = loopClient.Execute(loopRequest);
-                BallDontLiePlayerResponse loopResult = JsonConvert.DeserializeObject<BallDontLiePlayerResponse>(loopResponse.Content);
-                
+                BallDontLiePlayerResponse loopResult =
+                    JsonConvert.DeserializeObject<BallDontLiePlayerResponse>(loopResponse.Content);
+
                 players.AddRange(loopResult.data);
             }
-            
+
+            _players = players;
+
             return players;
         }
-        
+
         public IEnumerable<Player> GetPlayersByTeam(string teamName)
         {
-            List<Player> players = GetPlayers().ToList();
+            List<Player> players;
+            
+            players = _players == null ? GetPlayers().ToList() : _players.ToList();
 
-            List<Player> playersForTeam = new List<Player>();
-            
-            foreach (Player player in players)
-            {
-                if (player.team != null)
-                {
-                    if (player.team.full_name.ToLower().Contains(teamName.ToLower()) ||
-                        player.team.abbreviation.ToLower().Contains(teamName.ToLower()) ||
-                        player.team.city.ToLower().Contains(teamName.ToLower()) ||
-                        player.team.name.ToLower().Contains(teamName.ToLower()))
-                    {
-                        playersForTeam.Add(player);
-                    }
-                }
-            }
-            
-            return playersForTeam.OrderBy( p => p.last_name);
+            List<Player> playersForTeam = 
+                players.Where(player => player.team != null)
+                    .Where(player => player.team.full_name.ToLower().Contains(teamName.ToLower()) 
+                                     || player.team.abbreviation.ToLower().Contains(teamName.ToLower()) 
+                                     || player.team.city.ToLower().Contains(teamName.ToLower()) 
+                                     || player.team.name.ToLower().Contains(teamName.ToLower())).ToList();
+
+            return playersForTeam.OrderBy(p => p.last_name);
         }
-        
+
         //TODO: Add short, mid, long term???
         public SeasonStats GetShootingStats(Player player)
         {
             int id = player.id;
             List<SeasonStats> stats = new List<SeasonStats>();
-            
-            RestClient client = new RestClient($"https://www.balldontlie.io/api/v1/season_averages?season=2020&player_ids[]={player.id}") {Timeout = -1};
+
+            RestClient client =
+                new RestClient(
+                        $"https://www.balldontlie.io/api/v1/season_averages?season=2020&player_ids[]={player.id}")
+                    {Timeout = -1};
             client.UseNewtonsoftJson();
             RestRequest request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
 
-            BallDontLieSeasonStatsResponse result = JsonConvert.DeserializeObject<BallDontLieSeasonStatsResponse>(response.Content);
+            BallDontLieSeasonStatsResponse result =
+                JsonConvert.DeserializeObject<BallDontLieSeasonStatsResponse>(response.Content);
             stats.AddRange(result.data);
 
             return stats[0];
@@ -79,7 +84,7 @@ namespace nbaData
     public interface IBallDontLieManager
     {
         IEnumerable<Player> GetPlayers();
-        
+
         IEnumerable<Player> GetPlayersByTeam(string teamName);
 
         SeasonStats GetShootingStats(Player player);
