01684e630f3b7bce3f1946f83669e38a67ad9ffa
Zach Sarkis <zacherysarkis@gmail.com>
2021-07-18 15:01:42 -0600

    Add all playerIds to array to acquire stats

--- nba-data-front/src/shot-comparisons-page/shooter-data.js
+++ nba-data-front/src/shot-comparisons-page/shooter-data.js
@@ -1,6 +1,6 @@
+import Button from 'react-bootstrap/Button';
 import {useLocation} from 'react-router-dom';
 import { useEffect, useState } from "react";
-import Button from 'react-bootstrap/Button';
 import queryString from 'query-string';
 import axios from 'axios';
 
@@ -12,6 +12,8 @@ const ShooterData = () => {
   const teamB = params['teamB'];
   const [rosterA, setRosterA] = useState([]);
   const [rosterB, setRosterB] = useState([]);
+  const fullRoster = new Array(10);
+  const [selectedRoster, setSelectedRoster] = useState([]);
 
   useEffect(() => {
     const fetchTeams = async () => {
@@ -37,9 +39,65 @@ const ShooterData = () => {
     fetchTeams();
   }, []);
   
+  //add all selections to a list based off of position in array
+  //add button to search based off of contents of roster selections
+  //this implementation will have a bug if the selected value doesn't change.
+
   const handleClick = (e) => {
     //redirect
+    console.log('acquiring full shooting stats...');  
   }
+
+  const onPlayer0Change = (e) => {
+    const playerSelected = e.target.value;
+    fullRoster[0] = playerSelected;
+  }
+
+  const onPlayer1Change = (e) => {
+    const playerSelected = e.target.value;
+    fullRoster[1] = playerSelected;
+  }
+
+  const onPlayer2Change = (e) => {
+    const playerSelected = e.target.value;
+    fullRoster[2] = playerSelected;
+  }
+
+  const onPlayer3Change = (e) => {
+    const playerSelected = e.target.value;
+    fullRoster[3] = playerSelected;
+  }
+
+  const onPlayer4Change = (e) => {
+    const playerSelected = e.target.value;
+    fullRoster[4] = playerSelected;
+  }
+
+  const onPlayer5Change = (e) => {
+    const playerSelected = e.target.value;
+    fullRoster[5] = playerSelected;
+  }
+
+  const onPlayer6Change = (e) => {
+    const playerSelected = e.target.value;
+    fullRoster[6] = playerSelected;
+  }
+
+  const onPlayer7Change = (e) => {
+    const playerSelected = e.target.value;
+    fullRoster[7] = playerSelected;
+  }
+
+  const onPlayer8Change = (e) => {
+    const playerSelected = e.target.value;
+    fullRoster[8] = playerSelected;
+  }
+
+  const onPlayer9Change = (e) => {
+    const playerSelected = e.target.value;
+    fullRoster[9] = playerSelected;
+  }
+
 
   return ( 
     <div className="row mt-3">
@@ -47,35 +105,35 @@ const ShooterData = () => {
       Select Players:
     </div>
     <div className="col-md-4 mb-3">
-      <select className="form-select">
+      <select className="form-select" onChange={onPlayer0Change}>
         {rosterA.map((c) => (
           <option key={c.id} value={c.id}>
             {c.first_name} {c.last_name}
           </option>
         ))}
       </select>
-      <select className="form-select">
+      <select className="form-select" onChange={onPlayer1Change}>
         {rosterA.map((c) => (
           <option key={c.id} value={c.id}>
             {c.first_name} {c.last_name}
           </option>
         ))}
       </select>
-      <select className="form-select">
+      <select className="form-select" onChange={onPlayer2Change}>
         {rosterA.map((c) => (
           <option key={c.id} value={c.id}>
             {c.first_name} {c.last_name}
           </option>
         ))}
       </select>
-      <select className="form-select">
+      <select className="form-select" onChange={onPlayer3Change}>
         {rosterA.map((c) => (
           <option key={c.id} value={c.id}>
             {c.first_name} {c.last_name}
           </option>
         ))}
       </select>
-      <select className="form-select">
+      <select className="form-select" onChange={onPlayer4Change}>
         {rosterA.map((c) => (
           <option key={c.id} value={c.id}>
             {c.first_name} {c.last_name}
@@ -86,46 +144,47 @@ const ShooterData = () => {
       <p></p>
       Versus
       <p></p>
-    </div><select className="form-select">
+    </div>
+      <select className="form-select" onChange={onPlayer5Change}>
         {rosterB.map((c) => (
           <option key={c.id} value={c.id}>
             {c.first_name} {c.last_name}
           </option>
         ))}
       </select>
-      <select className="form-select">
+      <select className="form-select" onChange={onPlayer6Change}>
         {rosterB.map((c) => (
           <option key={c.id} value={c.id}>
             {c.first_name} {c.last_name}
           </option>
         ))}
       </select>
-      <select className="form-select">
+      <select className="form-select" onChange={onPlayer7Change}>
         {rosterB.map((c) => (
           <option key={c.id} value={c.id}>
             {c.first_name} {c.last_name}
           </option>
         ))}
       </select>
-      <select className="form-select">
+      <select className="form-select" onChange={onPlayer8Change}>
         {rosterB.map((c) => (
           <option key={c.id} value={c.id}>
             {c.first_name} {c.last_name}
           </option>
         ))}
       </select>
-      <select className="form-select">
-        {rosterB.map((c) => (
-          <option key={c.id} value={c.id}>
-            {c.first_name} {c.last_name}
-          </option>
-        ))}
-      </select>
+      <select className="form-select" onChange={onPlayer9Change}>
+      {rosterB.map((c) => (
+        <option key={c.id} value={c.id}>
+          {c.first_name} {c.last_name}
+        </option>
+      ))}
+    </select>
       <p></p>
     </div>
     <div className="text-center">
     <Button onClick={handleClick}>
-        Get Shooting Stats
+        Get Full Shooting Stats
       </Button>
     </div>
   </div>
