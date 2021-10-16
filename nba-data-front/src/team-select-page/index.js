50c815e6446fb76accced5dd51406b8d3e2feaa6
Zach Sarkis <zacherysarkis@gmail.com>
2021-10-16 15:58:31 -0600

    test FE deploy

--- nba-data-front/src/team-select-page/index.js
+++ nba-data-front/src/team-select-page/index.js
@@ -30,7 +30,7 @@ function App() {
       {/* TODO: figure out how to throw on incorrect path */}
       <Route exact path="/">
         <div className="container">
-          <Header subtitle = "Providing shooting metrics by matchup TEST" />
+          <Header subtitle = "Providing shooting metrics by matchup" />
           <Matchup allTeams={allTeams}/>
         </div>
       </Route>
